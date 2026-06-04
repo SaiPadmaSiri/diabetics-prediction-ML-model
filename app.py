e94bdf925e063612f2abce904714cd9115205b36
Sai padma siri S <saipadmasiri@gmail.com>
2026-06-04 12:57:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load trained model
+model = joblib.load("diabetes_model.pkl")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json()
+
+        features = [
+            float(data["pregnancies"]),
+            float(data["glucose"]),
+            float(data["bloodpressure"]),
+            float(data["skinthickness"]),
+            float(data["insulin"]),
+            float(data["bmi"]),
+            float(data["dpf"]),
+            float(data["age"])
+        ]
+
+        prediction = model.predict([features])
+
+        result = "Diabetes Detected" if prediction[0] == 1 else "No Diabetes"
+
+        return jsonify({
+            "prediction": result
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        })
+
+if __name__ == "__main__":
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
